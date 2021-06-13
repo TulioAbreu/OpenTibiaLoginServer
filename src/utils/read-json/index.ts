04418da927996635248fe8c573bf20bde5b63b91
tulioabreu <marco-tulioabreu1@hotmail.com>
2021-06-13 20:44:51 -0300

    Add read json util

---
+++ src/utils/read-json/index.ts
@@ -0,0 +1,10 @@
+import { readFileSync } from "fs";
+import { Maybe } from "../maybe"
+
+export function readJSONFileSync(filepath: string): Maybe<unknown> {
+    try {
+        return JSON.parse(readFileSync(filepath, "utf8"));
+    } catch (error) {
+        throw new Error(`Failed to read JSON file: ${error}`);
+    }
+}

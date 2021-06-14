064c880c81795ec02202dec3e94e9d47a000ee9d
tulioabreu <marco-tulioabreu1@hotmail.com>
2021-06-14 10:06:37 -0300

    Use Cam alias to config

--- src/services/cams/index.ts
+++ src/services/cams/index.ts
@@ -2,9 +2,11 @@ import { Character } from "../../db";
 import { Maybe } from "../../utils/maybe";
 import { config } from "../config";
 
+const CONFIG_CAMS_ENABLED = config.cams;
+
 class Cams {
     async get(name: string, _password: string): Promise<Maybe<Character[]>> {
-        if (config.cams !== true) {
+        if (!CONFIG_CAMS_ENABLED) {
             return null;
         }
         if (name.toLocaleLowerCase().indexOf("!cam") == 0) {
